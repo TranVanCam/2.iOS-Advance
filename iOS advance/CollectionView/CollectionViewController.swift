8be1a7df8f0e8d45e8b623a1fd69b9fbf91b7457
Cam Tran <tranvancam.bkdn@gmail.com>
2023-05-08 01:23:36 +0700

    1st collectionView commit

---
+++ iOS advance/CollectionView/CollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewController.swift
+//  iOS advance
+//
+//  Created by Cam Tran on 5/7/23.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+    
+    @IBOutlet weak var headTitle: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
